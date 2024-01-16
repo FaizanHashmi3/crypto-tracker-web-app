1b081051b2eb77af7208b2f2b4d9515eddd64ebb
FaizanHashmi3 <96041026+FaizanHashmi3@users.noreply.github.com>
2024-01-17 00:25:32 +0530

    Update index.js

--- src/components/Compare/SelectCoins/index.js
+++ src/components/Compare/SelectCoins/index.js
@@ -53,7 +53,7 @@ function SelectCoins({crypto1,crypto2,handleCoinChange}) {
             sx={styles}
            value={crypto1}
           label="Crypto 1"
-          onChange={(event)=>handleCoinChange(event,true)}
+          onChange={(event)=>handleCoinChange(event,false)}
     >
         
     {allCoins.filter((item)=> item.id != crypto2).map((coin,i)=>(
